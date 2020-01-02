773c59d61c8c2a355c8217f97d12cc9a4446bdab
spirrello <spirrello@opentext.com>
2020-01-02 14:34:14 -0500

    scaling limits

--- gke/main.tf
+++ gke/main.tf
@@ -16,5 +16,6 @@ module "cluster01" {
   subnetwork = "k8s-10-0-0-0-24"
   maintenance_start = "03:00"
   cluster_ipv4_cidr_block = ""
-
+  cluster_autoscaling_cpu_max = 12
+  cluster_autoscaling_memory_max = 32
 }
