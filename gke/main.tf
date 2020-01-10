cf2b4e2a5ad68aa16fd969f17554cc39ef4aaf89
spirrello <spirrello@opentext.com>
2020-01-09 20:26:47 -0500

    resort and disable istio

--- gke/main.tf
+++ gke/main.tf
@@ -11,25 +11,29 @@ variable "office_ip" {
 module "cluster01" {
   source = "github.com/spirrello/terraform-gcp.git//modules/gke"
 
+  #don't install istio by default
+  istio_config = "true"
   project = "${var.project}"
   region = "${var.region}"
   cluster_name = "${var.project}-01"
-  disable_public_endpoint = false
-  description = "Testing apps on K8s"
-  max_pods_per_node = "50"
-  general_purpose_machine_type = "n1-standard-4"
-  general_purpose_min_node_count = 2
-  general_purpose_max_node_count = 4
-  tags = ["cluster01"]
-  network = "k8s"
-  master_ipv4_cidr_block = "172.16.0.0/28"
-  subnetwork = "k8s-10-0-0-0-24"
-  maintenance_start = "03:00"
   cluster_ipv4_cidr_block = ""
   cluster_autoscaling_cpu_min = 6
   cluster_autoscaling_memory_min = 22
   cluster_autoscaling_cpu_max = 12
   cluster_autoscaling_memory_max = 32
+  description = "Testing apps on K8s"
+  disable_public_endpoint = false
+  general_purpose_machine_type = "n1-standard-4"
+  general_purpose_min_node_count = 2
+  general_purpose_max_node_count = 4
+  maintenance_start = "03:00"
+  master_ipv4_cidr_block = "172.16.0.0/28"
+  max_pods_per_node = "50"
+  network = "k8s"
+  subnetwork = "k8s-10-0-0-0-24"
+  tags = ["cluster01"]
+
+
   master_authorized_networks_config = [{
     cidr_blocks = [{
       cidr_block   = "10.0.0.0/8"
