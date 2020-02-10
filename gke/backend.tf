679f4b44a8acb94360f1b41547cc8fe7e5451c03
Stefano Pirrello <spirrello.com>
2020-02-10 18:51:44 -0500

    updated provider and backend

---
+++ gke/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "gcs" {
+    bucket = "tfbucket-257315"
+    prefix = "tfstate/gke"
+    credentials = "account.json"
+  }
+}
--- gke/provider.tf
+++ gke/provider.tf
@@ -1,22 +1,14 @@
-
-terraform {
-  backend "gcs" {
-    bucket = "tfbucket-257315"
-    prefix = "gke-dev/terraform.tfstate"
-    credentials = "account.json"
-  }
-}
-
 provider "google-beta" {
-  credentials = "${file("account.json")}"
-  project     = "${var.project}"
-  region      = "${var.region}"
+  credentials = file("account.json")
+  project     = var.project
+  region      = var.region
   version = "~> 3.3"
 }
-#commenting out to test Istio deployment
-# provider "google" {
-#   credentials = "${file("account.json")}"
-#   project     = "${var.project}"
-#   region      = "${var.region}"
-#   version = "~> 2.16"
-# }
+
+
+provider "google" {
+  credentials = file("account.json")
+  project     = var.project
+  region      = var.region
+  version = "~> 2.16"
+}
